2063d4a33562f2d41013504c41df484c2f9aa1f2
sreek <sreek@192.168.1.16>
2018-08-06 14:51:03 -0230

    starting spring

--- web/src/main/java/com/spring/testing/stock/web/WebApplication.java
+++ web/src/main/java/com/spring/testing/stock/web/WebApplication.java
@@ -1,12 +1,12 @@
-package com.spring.testing.stock.dbservice;
+package com.spring.testing.stock.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DbServiceApplication {
+public class WebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DbServiceApplication.class, args);
+		SpringApplication.run(WebApplication.class, args);
 	}
 }
--- web/src/test/java/com/spring/testing/stock/web/WebApplicationTests.java
+++ web/src/test/java/com/spring/testing/stock/web/WebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spring.testing.stock.dbservice;
+package com.spring.testing.stock.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DbServiceApplicationTests {
+public class WebApplicationTests {
 
 	@Test
 	public void contextLoads() {
