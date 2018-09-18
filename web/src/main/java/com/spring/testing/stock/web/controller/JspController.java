992ac3cf2906400e87a2ebec6af9cc5249a330de
sreek <sreek@192.168.1.16>
2018-09-18 16:17:21 -0230

    REST Implemented

--- web/src/main/java/com/spring/testing/stock/web/controller/JspController.java
+++ web/src/main/java/com/spring/testing/stock/web/controller/JspController.java
@@ -1,11 +1,16 @@
 package com.spring.testing.stock.web.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.testing.stock.web.dao.User;
@@ -64,6 +69,7 @@ public class JspController {
 	}
 
 	@RequestMapping(value = "/getUser")
+	@ResponseBody
 	public ModelAndView Home(@RequestParam int id) {
 
 		ModelAndView mv = new ModelAndView("userdetails");
@@ -73,4 +79,23 @@ public class JspController {
 		return mv;
 
 	}
+	
+	@RequestMapping(value = "/getAllUser")
+	@ResponseBody
+	public List<User> getAllUser() {
+
+		
+		return repo.findAll();		
+
+	}
+	
+	@RequestMapping(value = "/getUserByID/{id}")
+	@ResponseBody
+	public Optional<User> getUserById( @PathVariable("id") int id) {
+
+	return repo.findById(id);
+
+	}
+	
+	
 }
--- web/src/main/java/com/spring/testing/stock/web/reprository/LoginRepo.java
+++ web/src/main/java/com/spring/testing/stock/web/reprository/LoginRepo.java
@@ -1,9 +1,9 @@
 package com.spring.testing.stock.web.reprository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.spring.testing.stock.web.dao.User;
 
-public interface LoginRepo extends CrudRepository<User, Integer>{
+public interface LoginRepo extends JpaRepository<User, Integer>{
 
 }
